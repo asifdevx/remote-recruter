5da70b4658d79dc56d53f9ae6444a66b760a7751
asifdevx <xashadulasif@gmail.com>
2026-06-17 20:14:06 +0600

    initiat

--- src/utils/getErrorMsg.ts
+++
@@ -1,8 +0,0 @@
-   export const getErrMsg = (e: unknown): string | undefined => {
-        if (typeof e === "string") return e;
-        if (e && typeof e === "object") {
-          const maybe = e as { response?: { data?: { message?: string } }; data?: { message?: string } };
-          return maybe.response?.data?.message ?? maybe.data?.message;
-        }
-        return undefined;
-      };
